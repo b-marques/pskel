c9f54b4dd73f1bd8b1ed111d8fd101cab891dc8c
Bruno Marques <brunomn95@gmail.com>
2018-01-11 11:15:56 -0200

    fixing running tests

--- apps/jacobi/run.sh
+++ apps/jacobi/run.sh
@@ -1,5 +1,5 @@
 MPPADIR=/usr/local/k1tools
 
 $MPPADIR/bin/k1-power -gpdf -o tmp/ --period=20 -- $MPPADIR/bin/k1-jtag-runner --multibinary=output/bin/multibin_bin.mpk --exec-multibin=IODDR0:io_bin -- $1 $2 $3 $4 $5 $6 $7 $8
-mv tmp/* ../EnergyMPPATest/GolAPI/S$1/T$3/C$7
+mv tmp/* ../EnergyMPPATest/JacobiAPI/S$1/T$3/C$7
 rm -r tmp/
