05878e5cbe044a4438e339705ca136508dd642a7
marquesb <brunomn95@gmail.com>
2018-02-09 03:12:43 +0100

    updating tests scripts

--- apps/jacobi/run.sh
+++ apps/jacobi/run.sh
@@ -1,5 +1,6 @@
 MPPADIR=/usr/local/k1tools
 
 $MPPADIR/bin/k1-power -gpdf -o tmp/ --period=20 -- $MPPADIR/bin/k1-jtag-runner --multibinary=output/bin/multibin_bin.mpk --exec-multibin=IODDR0:io_bin -- $1 $2 $3 $4 $5 $6 $7 $8
-mv tmp/* ../EnergyMPPATest/JacobiAPI/S$1/T$3/C$7
+mkdir -p tests/Experiments/jacobiExperimentsEUROPAR/energy$1$3$7/
+mv tmp/* tests/Experiments/jacobiExperimentsEUROPAR/energy$1$3$7/
 rm -r tmp/
